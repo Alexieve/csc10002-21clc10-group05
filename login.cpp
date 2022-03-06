40101de5b5f40abc0595167699195ba0d78dd03b
Vũ Minh Quỳnh <92004323+Alexieve@users.noreply.github.com>
2022-03-06 20:45:06 +0700

    Login functions
    This function include all functions relative to login account

---
+++ login.cpp
@@ -0,0 +1,23 @@
+#include "functionPrototype.h"
+
+bool checkLogin(Account* curAccount, string username0, string password0){
+    if (curAccount->data.username == username0
+        && curAccount->data.password == password0)
+        return true;
+    return false;
+}
+bool login(Account* head, Account* &curAccount){
+    string username0, password0;
+    cout << "Login!\n";
+    cout << "Username: ";
+    cin >> username0;
+    cout << "Password: ";
+    cin >> password0;
+
+    Account* cur = head;
+    while (cur && !checkLogin(cur, username0, password0))
+        cur = cur->next;
+    if (cur == NULL) return false;
+    curAccount = cur;
+    return true;
+}
